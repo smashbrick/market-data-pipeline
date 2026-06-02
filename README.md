10820fb6e577856d27c60a01764cadc0b53d29e9
Smashbrick <smashbrick@yahoo.com>
2026-06-03 05:21:42 +0530

    Connected to binance stream

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Steps to making the file
 
 Once its created then you do below to activate it
 venv\Scripts\Activate.ps1
+
+I tried polygon.io but it requires me to register and request an API key, for simplicity sake I've gone with binance since it has a websocket stream as well and was pretty easy to setup.
--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Market Data Pipeline starting...")
+import asyncio
+import json
+import websockets
+
+URL = "wss://stream.binance.com:9443/ws/btcusdt@trade"
+
+
+async def main():
+    async with websockets.connect(URL) as ws:
+        while True:
+            message = await ws.recv()
+            data = json.loads(message)
+
+            # print(
+            #     f"Symbol: {data['s']} | "
+            #     f"Price: {data['p']} | "
+            #     f"Qty: {data['q']}"
+            # )
+            print(data)
+
+
+asyncio.run(main())
