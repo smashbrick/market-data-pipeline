440024ef4a98108e47d813034bc22ae4c70fce40
Smashbrick <smashbrick@yahoo.com>
2026-06-06 04:11:26 +0530

    Worked on some normalization

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ Steps to making the file
 > python -m env env
 
 Once its created then you do below to activate it
-venv\Scripts\Activate.ps1
+venv\Scripts\Activate.ps1S
 
 I tried polygon.io but it requires me to register and request an API key, for simplicity sake I've gone with binance since it has a websocket stream as well and was pretty easy to setup.
+
+T vs E
+
+E is when Binance's server generated and sent the event to you
+T is when the trade actually matched on the exchange
+
+There's network and processing lag between a trade happening and Binance packaging it into an event. For backtesting you want to replay trades in the order they happened, not the order you received them. Hence T.
--- main.py
+++ main.py
@@ -1,22 +1,56 @@
 import asyncio
 import json
 import websockets
+from dataclasses import dataclass
+from decimal import Decimal
+
 
 URL = "wss://stream.binance.com:9443/ws/btcusdt@trade"
 
+@dataclass
+class Trade():
+  symbol: str
+  price: Decimal
+  quantity: int
+  timestamp: int
+  is_buyer_maker: bool
 
-async def main():
-    async with websockets.connect(URL) as ws:
+def normalize(raw: dict) -> Trade:
+  return Trade(
+    symbol = raw["s"],
+    price = Decimal(raw['p']),
+    quantity = Decimal(raw['q']),
+    timestamp = raw['T'],
+    is_buyer_maker=raw['m']
+
+  )
+
+
+async def connect():
+  while True:
+    try:
+      print("Connecting to websocket...")
+
+      async with websockets.connect(URL) as ws:
+        print("Connected")
+
         while True:
-            message = await ws.recv()
-            data = json.loads(message)
-
-            # print(
-            #     f"Symbol: {data['s']} | "
-            #     f"Price: {data['p']} | "
-            #     f"Qty: {data['q']}"
-            # )
-            print(data)
+          message = await ws.recv()
+          raw = json.loads(message)
+          trade = normalize(raw)
 
 
-asyncio.run(main())
+          print(trade)
+    
+    except websockets.ConnectionClosed as e:
+      print(f"Connection closed: {e}. Reconnecting...")
+
+    except Exception as e:
+      print(f"Unexpected error: {e}. Reconnecting")
+
+    finally:
+      print("Cleanup done (If needed)")
+   
+
+
+asyncio.run(connect())
